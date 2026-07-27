0c248c727873ab785d46b23e9c3d820904e9ed01
Gaye Altun <gayealtun2004@gmail.com>
2026-07-27 12:30:29 +0300

    Configure EF Core and add initial migration

---
+++ TaskTracking.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore; //Dbcontext kullanılacak 
+using TaskTracking.Entities.Models; //userları burada oluşturmuştuk 
+
+namespace TaskTracking.DataAccess.Context;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext( //constructor 
+        DbContextOptions<ApplicationDbContext> options ) //db ye nasıl bağlanacak 
+        : base (options) //bu ayarlari üst sinifim  olan dbcontexte gönder 
+        {} // boş çünkü constructor ın tek işi options ı üst sinifa göndermek
+
+        public DbSet<User> Users {get; set; }//user entitylerinden oluşan bir koleksiyonu
+}                                            // veritabanında yönet 
